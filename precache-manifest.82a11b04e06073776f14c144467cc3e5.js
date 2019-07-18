83eba1dd4d8f3da26ade975f4d3ccf4142216087
Shehzad <shehzad@attentive.ai>
2019-07-18 18:45:16 +0530

    Updates

---
+++ precache-manifest.82a11b04e06073776f14c144467cc3e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cf1a202f826dd70a193",
+    "url": "/static/js/main.0cf1a202.chunk.js"
+  },
+  {
+    "revision": "68afdad577ee459cc254",
+    "url": "/static/js/1.68afdad5.chunk.js"
+  },
+  {
+    "revision": "0cf1a202f826dd70a193",
+    "url": "/static/css/main.5fad3910.chunk.css"
+  },
+  {
+    "revision": "d658839b143cc94102f827a19b256612",
+    "url": "/index.html"
+  }
+];
